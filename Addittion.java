f9502f902057611692b6e60e62378662c4035912
219275041 <81517434+219275041@users.noreply.github.com>
2021-04-01 22:33:15 +0200

    Add files via upload

---
+++ Addittion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.calculate1;
+
+/**
+ *
+ * @author Sonwabiso Moni
+ * stdNum 219275041
+ */
+public class Addittion {
+    public static double sum(double number1,double number2){
+     return number1+number2;   
+    }
+    
+}
